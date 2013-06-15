317be39158f0155ffe384e53eb4898a99f4e9bf9
PitchBlecc <florianheinrich@t-online.de>
2013-06-15 16:53:00 +0200

    error message

--- src/main/java/yoloswagswag/swe_app/MainActivity.java
+++ src/main/java/yoloswagswag/swe_app/MainActivity.java
@@ -7,6 +7,9 @@ import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.content.Intent;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.lang.String;
 
 public class MainActivity extends Activity {
 
@@ -27,7 +30,14 @@ public class MainActivity extends Activity {
     /* Called when the user clicks the Send button */
     public void okMessage(View view) {
         Intent intent = new Intent(this, timeSelector.class);
-        startActivity(intent);
+        EditText codeEdit =(EditText) findViewById(R.id.codeEdit);
+        TextView error =(TextView) findViewById(R.id.errorMessage);
+        String code = codeEdit.getText().toString();
+        if(code.length()==5){
+            startActivity(intent);
+            error.setText("");
+        }
+        else error.setText("Ihr Code muss aus 5 Zeichen bestehen!");
 
     }
     
