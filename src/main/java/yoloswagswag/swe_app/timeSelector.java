a889fe52fcfc1f0588144bfe2c18e181ba768ebf
Stephan Doerfler <stephan.doerfler@st.ovgu.de>
2013-06-17 18:02:08 +0200

    Ideas for time-management and slight changes in corresponding activity

--- src/main/java/yoloswagswag/swe_app/timeSelector.java
+++ src/main/java/yoloswagswag/swe_app/timeSelector.java
@@ -1,29 +1,32 @@
 package yoloswagswag.swe_app;
 
+import android.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class timeSelector extends Activity {
 
     /* this is where the selected timeslots are stored */
-    int[][] timeSelector = new int[7][4];
+    Calendar[][] timeSelector = new Calendar[7][4];
 
     /* the currently selected day  - initialised with monday*/
-    int currentDay = 0;
+    Calendar currentDate = Calendar.getInstance();
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.time_selector);
 
     /* default settings */
-        for(int i=0;i < 7; i++){
-            timeSelector[i][0] = 1;
-            timeSelector[i][1] = 1;
-            timeSelector[i][2] = 1;
-            timeSelector[i][3] = 1;
+        for(int i=0;i < timeSelector.length; i++){
+            timeSelector[i][0] = ;
+            timeSelector[i][1] = ;
+            timeSelector[i][2] = ;
+            timeSelector[i][3] = ;
         }
     }
 
@@ -32,118 +35,45 @@ public class timeSelector extends Activity {
         startActivity(new Intent(this, chillActivity.class));
     }
 
+    public void daySelect(View view){
+        switch(view.getId()){
+            case (R.id.btnMon):
+        }
+    }
+
     public void timeSelect(View view){
-        int slot=0;
-        int time=0;
+
         switch(view.getId()){
             case R.id.btn09:
-                slot = 0;
-                time = 0;
                 break;
             case R.id.btn10:
-                slot = 0;
-                time = 1;
                 break;
             case R.id.btn11:
-                slot = 0;
-                time = 2;
                 break;
             case R.id.btn12:
-                slot = 0;
-                time = 3;
                 break;
             case R.id.btn13:
-                slot = 1;
-                time = 0;
                 break;
             case R.id.btn14:
-                slot = 1;
-                time = 1;
                 break;
             case R.id.btn15:
-                slot = 1;
-                time = 2;
                 break;
             case R.id.btn16:
-                slot = 2;
-                time = 0;
                 break;
             case R.id.btn17:
-                slot = 2;
-                time = 1;
                 break;
             case R.id.btn18:
-                slot = 2;
-                time = 2;
                 break;
             case R.id.btn19:
-                slot = 2;
-                time = 3;
                 break;
             case R.id.btn20:
-                slot = 3;
-                time = 0;
                 break;
             case R.id.btn21:
-                slot = 3;
-                time = 1;
                 break;
             case R.id.btn22:
-                slot = 3;
-                time = 2;
                 break;
             case R.id.btn23:
-                slot = 3;
-                time = 3;
                 break;
         }
-
-        int oldtime = timeSelector[currentDay][slot];
-
-        /* determine the slot */
-        switch(slot){
-            case 0:
-                switch(oldtime){
-                    case 0:
-                        /* btn09 umfaerben */
-                    case 1:
-
-                    case 2:
-
-                    case 3:
-                }
-            case 1:
-                switch(oldtime){
-                    case 0:
-
-                    case 1:
-
-                    case 2:
-
-                    case 3:
-                }
-            case 2:
-                switch(oldtime){
-                    case 0:
-
-                    case 1:
-
-                    case 2:
-
-                    case 3:
-                }
-            case 3:
-                switch(oldtime){
-                    case 0:
-
-                    case 1:
-
-                    case 2:
-
-                    case 3:
-                }
-        }
-
-        timeSelector[currentDay][slot] = time;
     }
 }
