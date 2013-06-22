3ccf89812c52180c120c8ab82e2168658c6a6640
PitchBlecc <florianheinrich@t-online.de>
2013-06-22 18:04:20 +0200

    write in .csv

--- src/main/java/yoloswagswag/swe_app/pollActivity.java
+++ src/main/java/yoloswagswag/swe_app/pollActivity.java
@@ -7,9 +7,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -21,6 +27,7 @@ public class pollActivity extends Activity {
     private SharedPreferences selectedTimesStorage;
     private Calendar nextAlarmTime;
     private Calendar currentTime;
+    private int i;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +36,7 @@ public class pollActivity extends Activity {
         lastTimeSett = getSharedPreferences(LAST_TIME_STORAGE, 0);
         selectedTimesStorage = getSharedPreferences("selectedTimesStorage",0);
         currentTime = new GregorianCalendar();
-        int i =0;
+        i =0;
         int slotHour = selectedTimesStorage.getInt("day"+(currentTime.get(Calendar.DAY_OF_WEEK)-1)+"slot"+i, 0);
         while(slotHour<=currentTime.get(Calendar.HOUR_OF_DAY)&&i<4)
         {
@@ -42,11 +49,15 @@ public class pollActivity extends Activity {
         nextAlarmTime = new GregorianCalendar();
         nextAlarmTime.set(Calendar.HOUR_OF_DAY, slotHour);
         nextAlarmTime.set(Calendar.MINUTE, 0);
+        nextAlarmTime.set(Calendar.SECOND, 0);
         //nextAlarmTime.add(Calendar.SECOND, 20);
 
-        int bla=nextAlarmTime.get(Calendar.HOUR_OF_DAY);
         TextView pollText = (TextView) findViewById(R.id.pollText);
-        pollText.setText(" "+bla);
+
+        SharedPreferences pastAlarmStorage=getSharedPreferences("pastAlarmsStorage",0);
+        SharedPreferences.Editor editor = pastAlarmStorage.edit();
+        editor.putInt("day", currentTime.get(Calendar.DAY_OF_WEEK));
+        editor.putInt("slot", i-1);
 
         if(lastTimeSett.contains("last")){
             pollText.setText(getString(R.id.pollText)+lastTimeSett.getString("last","XX:XX")+"Uhr?");
@@ -68,6 +79,22 @@ public class pollActivity extends Activity {
         AlarmManager alarmManager = (AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
         alarmManager.set(AlarmManager.RTC_WAKEUP, nextAlarmTime.getTimeInMillis(),pendingIntent);
 
+        EditText numberText = (EditText) findViewById(R.id.pollNrEdit);
+        EditText timeText = (EditText) findViewById(R.id.pollTimeEdit);
+        EditText minuteText = (EditText) findViewById(R.id.pollMinuteEdit);
+        SharedPreferences userCodeStorage=getSharedPreferences("userCodeStorage",0);
+        String code = userCodeStorage.getString("userCode",null);
+        currentTime = new GregorianCalendar();
+        try {
+            File dir = new File(Environment.getExternalStorageDirectory(),"PsychoTest");
+            File f = new File(dir, code+".csv");
+            FileWriter writer = new FileWriter(f ,true);
+            writer.write(code+";"+currentTime.get(Calendar.DAY_OF_MONTH)+"."+currentTime.get(Calendar.MONTH)+"."+currentTime.get(Calendar.YEAR)+";"+selectedTimesStorage.getInt("day"+(currentTime.get(Calendar.DAY_OF_WEEK)-1)+"slot"+(i-1), 0)+":00;"+currentTime.get(Calendar.HOUR_OF_DAY)+":"+currentTime.get(Calendar.MINUTE)+";0;"+numberText.getText()+";"+timeText.getText()+";"+minuteText.getText()+"\n");
+            writer.flush();
+            writer.close();
+        } catch (IOException e){
+            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+        }
 
         if (!this.isTaskRoot()){
             this.finish();
@@ -79,5 +106,19 @@ public class pollActivity extends Activity {
 
     public void cancelPoll(View view){
         /* ungültigen Wert in csv schreiben, neuen Alarm setzen, View schließen */
+        SharedPreferences userCodeStorage=getSharedPreferences("userCodeStorage",0);
+        String code = userCodeStorage.getString("userCode",null);
+        currentTime = new GregorianCalendar();
+        try {
+            File dir = new File(Environment.getExternalStorageDirectory(),"PsychoTest");
+            File f = new File(dir, code+".csv");
+            FileWriter writer = new FileWriter(f ,true);
+            writer.write(code+";"+currentTime.get(Calendar.DAY_OF_MONTH)+"."+currentTime.get(Calendar.MONTH)+"."+currentTime.get(Calendar.YEAR)+";"+selectedTimesStorage.getInt("day"+(currentTime.get(Calendar.DAY_OF_WEEK)-1)+"slot"+(i-1), 0)+":00;-77;1;-77;-77;-77\n");
+            writer.flush();
+            writer.close();
+        } catch (IOException e){
+            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+        }
+        this.finish();
     }
 }
